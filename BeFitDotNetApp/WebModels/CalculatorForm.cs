2fb78e9c0158f773cde54179587bc85272a6180f
DariaKrysiak <daria.krysiak@gmail.com>
2020-06-15 00:18:48 +0200

    Create CalcualtorForm WebModel

---
+++ BeFitDotNetApp/WebModels/CalculatorForm.cs
@@ -0,0 +1,13 @@
+﻿namespace BeFitDotNet.WebModels
+{
+    public class CalculatorForm
+    {
+        public int Age { get; set; } = 25;
+
+        public bool Sex { get; set; } = false;
+
+        public float Weight { get; set; } = 60f;
+
+        public float Height { get; set; } = 1.70f;
+    }
+}
